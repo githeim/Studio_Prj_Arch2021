d0202489fcac3a8b9573b98875990debca1aa2a0
kwkim5 <kwkim5@gmail.com>
2021-06-22 12:50:55 +0900

    Update NetworkInterface.cpp

--- LgSwArchCode2021Distv2/LgFaceRecDemoTCP_Jetson_Nano/src/NetworkInterface.cpp
+++ LgSwArchCode2021Distv2/LgFaceRecDemoTCP_Jetson_Nano/src/NetworkInterface.cpp
@@ -1,5 +1,12 @@
 #include "NetworkInterface.h"
 #include "TcpSendRecvJpeg.h"
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+void runJpegEncoding()
+{
+    NetworkInterface::GetInstance()->Hwnd_JpegEncoding();
+}
 
 void runNetworkInterface()
 {
@@ -12,6 +19,7 @@ NetworkInterface::NetworkInterface()
 {
     TcpConnectedPort = nullptr;
     g_bTranmitRunFlag = false;
+    g_bStopWhenEmpty = false;
 }
 
 NetworkInterface::~NetworkInterface()
@@ -53,6 +61,8 @@ int NetworkInterface::Initialize(short listen_port)
     // :x: Create Thread
     g_bTranmitRunFlag = true;
     g_pThrTransmit = std::thread(runNetworkInterface);
+
+    g_pThrJpegEncoding = std::thread(runJpegEncoding);
     printf("\033[1;33m[%s][%d] :x: Start \033[m\n",__FUNCTION__,__LINE__);
 
 }
@@ -60,38 +70,62 @@ int NetworkInterface::Initialize(short listen_port)
 void NetworkInterface::Stop()
 {
     g_bTranmitRunFlag = false;
+    g_mtxJpeg.lock();
+    g_CondJpeg.notify_all();
+    g_mtxJpeg.unlock();
+    g_mtxMat.lock();
+    g_CondMat.notify_all();
+    g_mtxMat.unlock();
     g_pThrTransmit.join();
 }
 
-void NetworkInterface::Hwnd_Transmit(    ) {
-    int sent_size;
+void NetworkInterface::StopWhenEmpty()
+{
+    g_bStopWhenEmpty = false;
+    g_mtxJpeg.lock();
+    g_CondJpeg.notify_all();
+    g_mtxJpeg.unlock();
+    g_mtxMat.lock();
+    g_CondMat.notify_all();
+    g_mtxMat.unlock();
+    g_pThrTransmit.join();
+}
+
+void NetworkInterface::Hwnd_JpegEncoding(    ) {
+    static  int init_values[2] = { cv::IMWRITE_JPEG_QUALITY, 20 };
+    std::vector<int> param (&init_values[0], &init_values[0]+2);
     clock_t begin;
     clock_t end;
     usleep(50000);
     while (g_bTranmitRunFlag) {
         cv::Mat data;
 
-        printf("\033[1;36m[%s][%d] :x: size = [%lu] \033[m\n",__FUNCTION__,__LINE__,g_listTransmit.size());
+        printf("\033[1;36m[%s][%d] :x: size = [%lu] \033[m\n",__FUNCTION__,__LINE__,g_listMat.size());
 
         {
-            std::unique_lock<std::mutex> lck(g_mtxTransmit);
+            std::unique_lock<std::mutex> lck(g_mtxMat);
 
-            if (g_listTransmit.size() == 0 )
+            if (g_listMat.size() == 0 )
             {
-                g_CondVar.wait(lck);
+                if (g_bStopWhenEmpty)
+                    break;
+                    
+                g_CondMat.wait(lck);
                 continue;
             }
-            data = g_listTransmit.front();
-            g_listTransmit.pop_front();
+            data = g_listMat.front();
+            g_listMat.pop_front();
         }
+
+        JpegEncodedData  jpegData;
 
         begin = clock();
-        if ((sent_size = TcpSendImageAsJpeg(TcpConnectedPort,data)) < 0)  {
-            printf("\033[1;31m[%s][%d] :x: Err \033[m\n",__FUNCTION__,__LINE__);
-        }
+        cv::imencode(".jpg", data, jpegData.sendbuff, param);
         end = clock();
 
-        printf("sending time:%lu ms sent size:%d\n", 1000*(end-begin)/CLOCKS_PER_SEC, sent_size);
+        PushJpegData(jpegData);
+        
+        printf("encoding time:%lu ms\n", 1000*(end-begin)/CLOCKS_PER_SEC);
     }
     CloseTcpConnectedPort(&TcpConnectedPort); // Close network port;
     CloseTcpListenPort(&TcpListenPort);  // Close listen port
@@ -100,13 +134,65 @@ void NetworkInterface::Hwnd_Transmit(    ) {
 
 }
 
+void NetworkInterface::Hwnd_Transmit(    ) {
+    unsigned int imagesize;
+    ssize_t sent_size;
+    clock_t begin;
+    clock_t end;
+    usleep(50000);
+    while (g_bTranmitRunFlag) {
+        JpegEncodedData data;
+
+        printf("\033[1;36m[%s][%d] :x: size = [%lu] \033[m\n",__FUNCTION__,__LINE__, g_listJpeg.size());
+
+        {
+            std::unique_lock<std::mutex> lck(g_mtxJpeg);
+
+            if (g_listJpeg.size() == 0 )
+            {
+                if (g_bStopWhenEmpty)
+                    break;
+                    
+                g_CondJpeg.wait(lck);
+                continue;
+            }
+            data = g_listJpeg.front();
+            g_listJpeg.pop_front();
+        }
+
+        begin = clock();
+        imagesize = htonl(data.sendbuff.size()); // convert image size to network format
+        if (WriteDataTcp(TcpConnectedPort,(unsigned char *)&imagesize,sizeof(imagesize))!=sizeof(imagesize)) {
+            printf("Error to send:\n");
+        }
+        sent_size = WriteDataTcp(TcpConnectedPort, data.sendbuff.data(), data.sendbuff.size());
+        end = clock();
+
+        printf("sending time:%lu ms sent size:%lu\n", 1000*(end-begin)/CLOCKS_PER_SEC, sent_size);
+    }
+    CloseTcpConnectedPort(&TcpConnectedPort); // Close network port;
+    CloseTcpListenPort(&TcpListenPort);  // Close listen port
+
+    printf("\033[1;33m[%s][%d] :x: Thread End \033[m\n",__FUNCTION__,__LINE__);
+
+}
+
+void NetworkInterface::PushJpegData(JpegEncodedData &jpegData) {
+    g_mtxJpeg.lock();
+
+    g_listJpeg.push_back(jpegData);
+
+    g_CondJpeg.notify_all();
+    g_mtxJpeg.unlock();
+}
+
 void NetworkInterface::PushDataToSend(cv::Mat &data) {
-    g_mtxTransmit.lock();
+    g_mtxMat.lock();
 
     // :x: copy to msg queue
-    g_listTransmit.push_back(data);
+    g_listMat.push_back(data);
     // mutex Unlock
 
-    g_CondVar.notify_all();
-    g_mtxTransmit.unlock();
+    g_CondMat.notify_all();
+    g_mtxMat.unlock();
 }
