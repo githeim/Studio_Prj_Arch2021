625f6ad121eff194a539d55913f96bfeac6efadd
windheim <windheim@nowhere.com>
2021-06-24 13:40:12 +0900

    Remote UI initial

---
+++ Remote_UI/r.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd build/Debug ; ./UI_Controller-121212.343456.out
+#cd build/Debug ; gdb UI_Controller-121212.343456.out
+
---
+++ Remote_UI/src/CRunModeDlg.h
@@ -0,0 +1,4 @@
+#ifndef  _CRUNMODEDLG_H_
+#define _CRUNMODEDLG_H_
+
+#endif // end #ifndef  _CRUNMODEDLG_H_
